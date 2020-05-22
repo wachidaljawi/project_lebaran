1152ae79d939bad91f875aa9df0e5472a644c54d
wachidaljawi <rwachid17@gmail.com>
2020-05-22 14:46:35 +0700

    create model,controller dan migration

---
+++ project_datatables/app/Http/Controllers/JabatanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\jabatan;
+use Illuminate\Http\Request;
+
+class JabatanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\jabatan  $jabatan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(jabatan $jabatan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\jabatan  $jabatan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(jabatan $jabatan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\jabatan  $jabatan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, jabatan $jabatan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\jabatan  $jabatan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(jabatan $jabatan)
+    {
+        //
+    }
+}
---
+++ project_datatables/app/Http/Controllers/PendidikanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\pendidikan;
+use Illuminate\Http\Request;
+
+class PendidikanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\pendidikan  $pendidikan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(pendidikan $pendidikan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\pendidikan  $pendidikan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(pendidikan $pendidikan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\pendidikan  $pendidikan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, pendidikan $pendidikan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\pendidikan  $pendidikan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(pendidikan $pendidikan)
+    {
+        //
+    }
+}
